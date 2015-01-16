7b6054fabb4f03f2a3200b593f3802d930e7e2e6
Sulman Baig <sulman.baig@ginevra.dibe.unige.it>
2015-01-16 17:36:54 +0100

    getting emotion estimation by log of gammaP and gammaN

--- testFile.m
+++ testFile.m
@@ -8,13 +8,13 @@ load('CONST_DATA.mat');
 clearvars DELTAT_N DELTAT_P TRANSITION_P TRANSITION_N
 
 % read first File from the path provided
-dataPath = strcat('C:\Users\sulman.baig\Documents\DATA\50-100','\');
+dataPath = strcat('C:\Users\sulman.baig\Documents\DATA\1-50','\');
 dataFiles = dir(strcat(dataPath,'*.dat'));
 [TimeStamp,IDx,Xi,Yi] = importFileData(strcat(dataPath,dataFiles(1).name));
 % %Zones% gets zones for all Xi and Yi values
 Zones = zoneAllXY(Xi, Yi, ZONE);
 % %index% = places where samples have data for 85 number zone
-index = find(IDx == 100);
+index = find(IDx == 97);
 % %zoneId% = zones trajectory for a particular id
 zoneId = Zones(index);
 % %changeZoneId% = zones without repetition of specific trajectory
@@ -37,10 +37,15 @@ flagPositive = checkPositiveTrajectory(POS_ZONE,changeZoneId);
 clearvars dataPath dataFiles Xi Yi TimeStamp Zones index zoneId indexChangeZone
 clearvars time4Trajectory timeChangerPoints IDx
 
-Gamma = zeros(size(changeZoneId));
-Gamma(1,1) = (1/183)*(1/2);
+GammaP = zeros(size(changeZoneId));
+GammaP(1,1) = (1/183)*(1/2);
+GammaN = zeros(size(changeZoneId));
+GammaN(1,1) = (1/183)*(1/2);
 for a=2:1:length(changeZoneId)
-    Gamma(a,1) = TRANSITION_P_NORM(changeZoneId(a-1,1),changeZoneId(a,1)) * DELTAT_P_NORM(changeZoneId(a-1,1),changeZoneId(a,1),quantizedTime(a-1,1)) * Gamma(a-1,1);
+    GammaN(a,1) = TRANSITION_N_NORM(changeZoneId(a-1,1),changeZoneId(a,1)) * DELTAT_N_NORM(changeZoneId(a-1,1),changeZoneId(a,1),quantizedTime(a-1,1)) * GammaN(a-1,1);
+     GammaP(a,1) = TRANSITION_P_NORM(changeZoneId(a-1,1),changeZoneId(a,1)) * DELTAT_P_NORM(changeZoneId(a-1,1),changeZoneId(a,1),quantizedTime(a-1,1)) * GammaP(a-1,1);
+
 end
+
 
 clearvars a POS_ZONE ZONE QUANTIZATION changeTime
