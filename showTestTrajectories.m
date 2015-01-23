083eea626b81e20ef8ab566c677f969d80c50cff
Sulman Baig <sulman.baig@ginevra.dibe.unige.it>
2015-01-23 16:55:28 +0100

    show for presentation

--- showTestTrajectories.m
+++ showTestTrajectories.m
@@ -13,13 +13,53 @@ function showTestTrajectories(filePath,fileNumber)
     
     showMap();
     
+    preZone = ones(100,1)*(-1);
+    preTime = ones(100,1)*(-1);
+    preprobP = ones(100,1)*(-1);
+    preprobN = ones(100,1)*(-1);
     
     for a=min(TimeStamp):1:max(TimeStamp)
         hold on;
         tIndex = find(TimeStamp == a);
         xtemp = Xi(tIndex);
         ytemp = Yi(tIndex);
-        scatter(xtemp,ytemp,'.');
+        idTemp = IDx(tIndex);
+        zTemp = Zones(tIndex);
+        for b=1:1:length(idTemp)
+            c = idTemp(b);
+            if zTemp(b)==0
+                continue;
+            else
+                if preZone(c)==-1
+                    preZone(c)=zTemp(b);
+                    preTime(c)=a;
+                    preprobP(c)=(1/183)*(1/2);
+                    preprobP(c)=(1/183)*(1/2);
+                else
+                    if zTemp(b)~= preZone(c)
+                        postTime = a-preTime(c);
+                        postTime = find(QUANTIZATION<=postTime,1,'last');
+                        preprobN(c) = TRANSITION_N_NORM(preZone(c),zTemp(b)) * DELTAT_N_NORM(preZone(c),zTemp(b),postTime) * preprobN(c);
+                        preprobP(c) = TRANSITION_P_NORM(preZone(c),zTemp(b)) * DELTAT_P_NORM(preZone(c),zTemp(b),postTime) * preprobP(c);
+                        colorvalue = preprobP/(preprobP+preprobN);
+                        preZone(c)=zTemp(b);
+                        preTime(c)=a;
+                        if colorvalue
+                            colorR = 1;
+                            colorG = 1;
+                        elseif colorvalue<0.5
+                            colorR = 1;
+                            colorG = colorvalue*2;
+                        else
+                            colorR = (colorvalue*2)-0.2;
+                            colorG = 1;
+                        end
+                    end
+                end
+            end
+            scatter(xtemp(b),ytemp(b),'.','MarkerEdgeColor',[1 1 0]);
+        end
+        
         hold off;
     end
     
